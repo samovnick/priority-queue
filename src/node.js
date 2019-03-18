7da6231aa1bca5466ccf24693717c09c3801a026
SAMOVENDYUK NIKOLAY <samovnick@gmail.com>
2019-03-18 04:59:35 +0300

    Add class Node

--- src/node.js
+++ src/node.js
@@ -1,23 +1,30 @@
 class Node {
-	constructor(data, priority) {
+  constructor(data, priority) {
+    this.data - data;
+    this.priority = priority;
+    this.child = null;
+  }
 
-	}
+  appendChild(node) {
+    this.child = node;
+  }
 
-	appendChild(node) {
+  removeChild(node) {
+    this.child = null;
+  }
 
-	}
+  remove() {
+    delete this.child;
+  }
 
-	removeChild(node) {
-
-	}
-
-	remove() {
-
-	}
-
-	swapWithParent() {
-		
-	}
+  swapWithParent() {
+    let temp = this.data;
+    this.data = this.child.data;
+    this.child.data = temp;
+    temp = this.priority;
+    this.priority = this.child.priority;
+    this.child.priority = temp;
+  }
 }
 
 module.exports = Node;
